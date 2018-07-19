fb653e21722a830cae263fe3dff0df5684bc773b
RakeshKumar <92.rakesh@gmail.com>
2018-07-19 22:31:55 +0530

    setup

---
+++ tutorial/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/rakeshkumar/Desktop/Django/django-rest-framework/django-rest-framework-1/tutorial/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
